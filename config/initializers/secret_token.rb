fcdfd16a7fda0242a95c9a11475ed79045660288
Justin Pollheim <mrpollheim@gmail.com>
2013-07-15 16:46:52 +0930

    I think we should both work on this onegit add .! Needs to be updated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook1::Application.config.secret_key_base = '081132356771b622952542cb7e383bffec57a9e0492e8044aeace0d7c57d845510f9b1da1ed2532b6c9ace28f6fe490cea794a96e745eabab7f20c41bfd22ffc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Treebook1::Application.config.session_store :cookie_store, key: '_treebook1_session'
